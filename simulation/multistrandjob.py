c71a604a78c684628ede338d23ef080caf5f5210
Joseph Berleant <jberleant@dna.caltech.edu>
2015-05-05 02:47:11 -0700

    Increased max simulations per iteration for automatic error reduction function for Multistrand jobs

--- simulation/multistrandjob.py
+++ simulation/multistrandjob.py
@@ -243,9 +243,9 @@ class MultistrandJob(object):
       else:
         reduction = error / goal
         num_trials = int(block.get_num_points() * (reduction**2 - 1) + 1)
-        num_trials = min(num_trials, 50)
+        num_trials = min(num_trials, 200)
         
-      self.run_simulations(int(math.ceil(num_trials/10.0)), 10)
+      self.run_simulations(int(math.ceil(num_trials/20.0)), 20)
       error = block.get_error()
       goal = max(abs_goal, rel_goal * block.get_mean())
     
