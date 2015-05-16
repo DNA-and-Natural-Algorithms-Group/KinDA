674ae19f0cbb17529d6ced24c093e89198cfcd9a
Joseph Berleant <jberleant@dna.caltech.edu>
2015-05-16 13:05:28 -0700

    Modified datablock for efficiency. Data is no longer stored in files by default. To save data from a session, use Datablock.export_data().

--- simulation/datablock.py
+++ simulation/datablock.py
@@ -1,15 +1,6 @@
 import os, atexit
 import random, math
 import options
-
-blocklist = [] # Stores active blocks for deletion at program exit
-
-# Define a cleanup function and register for execution when Python exits.
-@atexit.register
-def cleanup_all():
-  for block in blocklist[:]:
-    block.cleanup()
-
   
 # Define default statistics functions    
 def default_mean_func(datablock):
@@ -44,39 +35,26 @@ class Datablock(object):
   quantity. Allows calculation of mean, standard deviation,
   and error bars, with optional functions to calculate each."""
   
+  id_counter = 0
+
   id = None
-  filepath = None
   datatype = None
   
-  open = False
-  data = None
+  data = []
   
   mean_func = None
   std_func = None
   error_func = None
   
-  cleaned = False
-  
   def __init__(self, datatype = 'float',
               mean_func = None, std_func = None, error_func = None):
     """Creates a new Datablock object with a unique
-    id and creates the associated file. Assigns the error
-    function, if given, or the default function, if none is given."""
+    id. Assigns the statistics functions, if given, or the default functions, if not."""
     
-    # Check that temp file directory exists
-    base_dir = os.path.join(os.curdir, "temp")
-    if not os.path.exists(base_dir):
-      os.mkdir(base_dir)
-    
-    # Create unique unused id
-    self.id = random.randrange(0,9999999)
-    self.filepath = os.path.join(base_dir, "tmp" + str(self.id))
-    while os.path.exists(self.filepath):
-      self.id = random.randrange(0,9999999)
-      self.filepath = os.path.join(base_dir, "tmp" + str(self.id))
+    # Assign unique id and increment global id counter
+    self.id = Datablock.id_counter
+    Datablock.id_counter += 1
       
-    open(self.filepath, "w").close() # Create file
-    
     # Set datatype
     if datatype != 'float' and datatype != 'int' and datatype != 'string':
       print "WARNING: Invalid datatype %s" % datatype
@@ -87,60 +65,27 @@ class Datablock(object):
     self.set_mean_func(mean_func)
     self.set_std_func(std_func)
     self.set_error_func(error_func)
-    
-    # Add to blocklist (for deletion if Python exits badly)
-    blocklist.append(self)
-    
-  
-  def __del__(self):
-    """Cleans up by calling cleanup()."""
-    if not self.cleaned:
-      self.cleanup()
-  
-  ## Opening/Closing
-  #  Blocks may be opened and closed explicitly for improved efficiency.
-  #  If a block is not explicitly opened, it will close automatically
-  #  after each operation.
-  #  Note that even in an open block, the data file will always be up-to-date.
-  def open_block(self):
-    if not self.open:
-      self.data = self.read_data()
-      self.open = True
-  def close_block(self):
-    self.data = None
-    self.open = False
-  def read_data(self):
-    with open(self.filepath, "r") as f:
-      if self.datatype == 'float':
-        vals = [float(line) for line in f.readlines()]
-      elif self.datatype == 'int':
-        vals = [int(line) for line in f.readlines()]
-      elif self.datatype == 'string':
-        vals = [line.strip() for line in f.readlines()]
-      else:
-        assert False, "Invalid datatype %s" % self.datatype
-    return vals
+
   
   ## Data management
   def add_data(self, vals):
     """Appends the given values to data already stored with this block."""
-    with open(self.filepath, "a") as f:
-      for v in vals:
-        f.write(repr(v) + "\n")
-    if self.open:
-      data.append(vals)
+    data.append(vals)
     
   def get_data(self):
     """Returns all data stored with this block. This may be a lot..."""
-    if self.open:
-      return self.data
-    else:
-      return self.read_data()
+    return self.data
     
   def clear_data(self):
-    open(self.filepath, "w").close()
-    if self.open:
-      self.data = []
+    self.data = []
+
+  def export_data(self, path):
+    f = open(path, 'w')
+    print "Exporting data in datablock {0}".format(self.id)
+    for d in data:
+      f.write("{0}\n".format(d))
+    f.close()
+    print "Data export complete!"
   
   ## Data manipulation/statistics
   def get_num_points(self):
@@ -174,11 +119,3 @@ class Datablock(object):
     else:
       self.error_func = error_func
   
-  def cleanup(self):
-    """If options.flags['no-cleanup'] is not set, deletes
-    the associated file. Otherwise, nothing is done.
-    The datablock object should not be handled after calling this function"""
-    if not options.flags['no-cleanup']:
-      os.remove(self.filepath)
-    blocklist.remove(self)
-    self.cleaned = True
