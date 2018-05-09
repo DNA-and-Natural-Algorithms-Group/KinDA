4555c2067549446af1b0f0e61b6faa15b36b440e
Joseph Berleant <jberleant@gmail.com>
2018-05-08 22:02:48 -0400

    Added default sodium/magnesium concentrations for nupack and Multistrand. Uncommented default options for Peppercorn.

--- kinda/options.py
+++ kinda/options.py
@@ -1,5 +1,5 @@
 kinda_params = {
-  'stop_macrostate_mode': 'count-by-complex', # may be 'disassoc', 'count-by-complex', 'count-by-domain'
+  'stop_macrostate_mode': 'disassoc', # may be 'disassoc', 'count-by-complex', 'count-by-domain'
   'start_macrostate_mode': 'disassoc', # may be 'disassoc', 'count-by-complex', 'count-by-domain'
   'multistrand_similarity_threshold': 0.51,
   'nupack_similarity_threshold': 0.51,
@@ -19,6 +19,8 @@ multistrand_params = {
   'substrate_type': 'DNA',
   'rate_method': 'Metropolis',
   'temperature': 25,
+  'sodium': 1.0,
+  'magnesium': 0.0,
   'unimolecular_scaling': 5.0e6,
   'bimolecular_scaling': 1.4e6,
   'join_concentration': 1e-15 # this must be low enough to ensure no bind reactions occur after the first step of a simulation
@@ -29,17 +31,19 @@ multistrand_params = {
 nupack_params = {
   'dangles': 'some',
   'material': 'dna',
-  'T': 25
+  'T': 25,
+  'sodium': 1.0,
+  'magnesium': 0.0
 }
 
 # These defaults are given directly to Peppercorn, unless overridden
 # while initializing a System object
 peppercorn_params = {
-#  'max_complex_size': 6,
-#  'max_reaction_count': 1000,
-#  'max_complex_count': 200,
-#  'release_cutoff_1_1': 8,
-#  'release_cutoff_1_N': 8,
-#  'remote_migration': True,
-#  'max_helix_migration': True
+  'max_complex_size': 6,
+  'max_reaction_count': 1000,
+  'max_complex_count': 200,
+  'release_cutoff_1_1': 8,
+  'release_cutoff_1_N': 8,
+  'remote_migration': True,
+  'max_helix_migration': True
 }
