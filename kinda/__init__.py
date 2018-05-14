2b09775f007ed86f23440cc955ace6bec505d846
Joseph Berleant <jberleant@gmail.com>
2018-05-14 21:58:49 +0000

    fixed NUPACKHOME environment variable check in kinda/__init__.py

--- kinda/__init__.py
+++ kinda/__init__.py
@@ -13,6 +13,6 @@ import enumeration
 # Check that NUPACKHOME environment variable is set and 'sample' executable is available there.
 # If not, print a warning message.
 import os
-if 'NUPACKHOME' not in os.environ or not os.path.isfile(os.path.join(os.environ['NUPACKHOME'], 'bin', 'sample')):
+if 'NUPACKHOME' not in os.environ or not os.path.isfile(os.path.join(os.environ['NUPACKHOME'], 'build', 'bin', 'sample')):
   print "WARNING: NUPACKHOME environment variable may not be set properly. If simulations fail, make sure NUPACKHOME is set to the directory containing your Nupack executables and that the 'sample' executable exists."
 del os
